042e6cefb8426811daf68c098a6c13b1cb39d36e
farhatizakaria <zakariafarhati18@gmail.com>
2020-03-14 17:47:10 +0000

    'master'

--- login-bot.py
+++ login-bot.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello World") # This is just for jocking, So I am going to begining develop the script
+from selenium import webdriver
+from time import sleep
+from random import random as rnd
 
+emailTest = None # Write here your email
+pw = 0000 # Also your password
 
-if __name__ == '__main__':
-    main()
+class Bot:
+    def __init__(self,email,password):
+        self.email = email
+        self.password = password
+        self.driver = webdriver.Firefox()
+        self.driver.get("https://www.facebook.com/")
+        sleep(rnd())
+        self.driver.find_element_by_xpath('//*[@id="email"]').click()
+        sleep(rnd())
+        self.driver.find_element_by_xpath('//*[@id="email"]').send_keys(emailTest)
+        sleep(rnd())
+        self.driver.find_element_by_xpath('//*[@id="pass"]').click()
+        sleep(rnd())
+        self.driver.find_element_by_xpath('//*[@id="pass"]').send_keys(pw)
+        sleep(rnd())
+        self.driver.find_element_by_xpath('//*[@id="loginbutton"]').click()
+
+Bot(emailTest,pw)
